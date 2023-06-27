7c27391609aeded0c578e2bd5aa6153aa5ffc7ec
Distant22 <sindistant@gmail.com>
2023-06-27 22:48:41 +0800

    Adjust Assignment 6

---
+++ Week-1/Assignment-6/index.js
@@ -0,0 +1,15 @@
+function delayedResultPromise(n1, n2, delayTime) {
+    // your code here
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(n1 + n2);
+        }, delayTime);
+    });
+}
+
+async function main() {
+    const result = await delayedResultPromise(4, 5, 3000);
+    console.log(result);
+}
+
+main();
